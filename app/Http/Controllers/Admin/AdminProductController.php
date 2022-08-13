8cd45d35146151027d1c5ffd4acdab023f824f9b
Aliyu <aliyubababello@gmail.com>
2022-08-13 13:46:10 +0100

    laravel/ui lib addedd

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -19,13 +19,8 @@ class AdminProductController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            "name" => "required|max:255",
-            "description" => "required",
-            "price" => "required|numeric|gt:0",
-            'image' => 'image',
-        ]);
-
+        Product::validate($request);
+       
         $newProduct = new Product();
         $newProduct->setName($request->input('name'));
         $newProduct->setDescription($request->input('description'));
