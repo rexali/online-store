864f5c0c5c96cc1eb9085cf6579e068022a99e03
Aliyu <aliyubababello@gmail.com>
2022-08-13 05:55:16 +0100

    mysqldb created and populated

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
-@section('title', $title)
-@section('subtitle', $subtitle)
+@section('title', $viewData['title'])
+@section('subtitle', $viewData['subtitle'])
 @section('content')
-<div class="container">
-<div class="row">
-<div class="col-lg-4 ms-auto">
-<p class="lead">{{ $description }}</p>
-</div>
-<div class="col-lg-4 me-auto">
-<p class="lead">{{ $author }}</p>
-</div>
-</div>
-</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 ms-auto">
+                <p class="lead">{{ $viewData['description'] }}</p>
+            </div>
+            <div class="col-lg-4 me-auto">
+                <p class="lead">{{ $viewData['author'] }}</p>
+            </div>
+        </div>
+    </div>
 @endsection
